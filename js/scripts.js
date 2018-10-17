673b375cf67f427034adce9e2d109292b4316308
Brooke Perry and Paige Williams <brooke@>
2018-10-17 11:53:46 -0700

    Separate front-end and back-end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
-
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number: "));
+//Back-end logic
 
 var add = function(number1, number2) {
 	return number1 + number2;
@@ -18,14 +16,31 @@ var divide = function(number1, number2) {
 	return number1 / number2;
 };
 
-var result = add(number1, number2);
-alert(result);
+//Front-end logic
 
-var result = subtract(number1, number2);
-alert(result);
+$(document).ready(function(){
+	var number1 = parseInt(prompt("Enter a number:"));
+	var number2 = parseInt(prompt("Enter another number: "));
 
-var result = multiply(number1, number2);
-alert(result);
+	alert(add(number1, number2));
 
-var result = divide(number1, number2);
-alert(result);
+});
+
+
+
+
+
+
+
+
+// var result = add(number1, number2);
+// alert(result);
+//
+// var result = subtract(number1, number2);
+// alert(result);
+//
+// var result = multiply(number1, number2);
+// alert(result);
+//
+// var result = divide(number1, number2);
+// alert(result);
