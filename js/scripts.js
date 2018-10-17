ffa24e7cb4e9866e85e146b7f27fdf05c62795b9
Brooke Perry and Paige Williams <brooke@>
2018-10-17 16:16:00 -0700

    Make columns and style buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -35,6 +35,14 @@ $(document).ready(function(){
 		$("#outputSubtract").text(result);
 	});
 
+	$("form#multiply").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#multiply1").val());
+		var number2 = parseInt($("#multiply2").val());
+		var result = divide(number1, number2);
+		$("#outputMultiply").text(result);
+	});
+
 	$("form#divide").submit(function(event){
 		event.preventDefault();
 		var number1 = parseInt($("#divide1").val());
